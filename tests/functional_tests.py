a57e2ce5fef52c9671d303950820cd1fb6fa8acf
Nathan Baja <nathanflach@hotmail.com>
2023-02-17 00:53:06 -0300

    Add new list item test

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item.'
+            'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -39,22 +39,31 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_tablee')
-        rows = self.browser.find_element(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = self.browser.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting here to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical).
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
 
         # The page updates again, and now shows both items on her list.
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = self.browser.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
+        )
 
         # Edith wonders if the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
     # She visits that URL - her to-do list is still there.
 
